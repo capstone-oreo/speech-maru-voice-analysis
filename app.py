c823d60fe5bde75ceafd1d8a8d517a3badbecd7a
dohyeon-han <limited.hyeon@gmail.com>
2023-04-27 23:50:21 +0900

    feat: 배포 확인용 api 구현

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ import tempfile
 app = Flask(__name__)
 api = Api(app)
 
+
+# 배포 확인용
+@api.route('/test')
+class Test(Resource):
+    def get(self):
+        return 'flask'
+
+
 # 업로드한 파일을 바로 다운로드한다.
 @api.route('/upload')
 class Upload(Resource):
@@ -17,7 +25,7 @@ class Upload(Resource):
         with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
             file.save(tmp_file.name)
             return send_file(tmp_file.name, attachment_filename=file.filename, as_attachment=True,
-                         mimetype=file.content_type)
+                             mimetype=file.content_type)
 
 
 if __name__ == '__main__':
