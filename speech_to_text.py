afc99548f213bef2298d6d5d038bdf7ea3992739
dohyeon-han <limited.hyeon@gmail.com>
2023-05-01 23:41:23 +0900

    feat: access token을 지역 변수로 변경하고 return 하도록 변경

--- speech_to_text.py
+++ speech_to_text.py
@@ -8,18 +8,18 @@ load_dotenv()
 
 class STT:
     def __init__(self):
-        self.access_token = None
         self.client_id = os.getenv("CLIENT_ID")
         self.client_secret = os.getenv("CLIENT_SECRET")
         self.redis_client = redis.Redis(host='redis', port=6379, db=0)
 
     # redis에서 token을 찾고 없으면 api 요청 후 redis에 저장한다.
     def get_access_token(self):
-        self.access_token = self.redis_client.get('access_token')
-        if self.access_token is None:
+        access_token = self.redis_client.get('access_token')
+        if access_token is None:
             FIVE_HOUR = 60 * 60 * 5
             data = {'client_id': self.client_id, 'client_secret': self.client_secret}
             response = requests.post('https://openapi.vito.ai/v1/authenticate', json=data)
-            self.access_token = response.json()['access_token']
-            self.redis_client.set('access_token', self.access_token)
+            access_token = response.json()['access_token']
+            self.redis_client.set('access_token', access_token)
             self.redis_client.expire('access_token', FIVE_HOUR)
+        return access_token
