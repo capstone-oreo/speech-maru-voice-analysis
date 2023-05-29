f9b58a73a856162932e4f99ca9f2ff4982cde4eb
dohyeon-han <limited.hyeon@gmail.com>
2023-05-29 17:34:11 +0900

    feat: SttResponse 구현 해당 객체를 json으로 반환한다.

---
+++ stt_response.py
@@ -0,0 +1,8 @@
+class SttResponse:
+    def __init__(self, text=[], speed=[], volume=[], keyword=[], textInfo=[], habitualWord=[]):
+        self.text = text
+        self.speed = speed
+        self.volume = volume
+        self.keyword = keyword
+        self.textInfo = textInfo
+        self.habitualWord = habitualWord
