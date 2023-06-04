d3f34a0cffa96ad7dcb0aa645532c5310a5ba87c
soojin <sobeast742@gmail.com>
2023-06-04 19:24:39 +0900

    refactor : get_transcribe_msg_by_id_list return 변수 수정

--- speech_to_text.py
+++ speech_to_text.py
@@ -15,8 +15,7 @@ class Stt:
         self.client_id = os.getenv("VITO_CLIENT_ID")
         self.client_secret = os.getenv("VITO_CLIENT_SECRET")
         self.redis_client = redis.Redis(host=os.getenv("REDIS_HOST"), port=6379, db=0)
-        self.stt_response=stt_response.SttResponse()
-
+    
     # 파일을 Vito API에 전송하고 id를 받는다.
     def get_transcribe_id(self, voice_file):
         access_token = self.__get_access_token()
@@ -32,20 +31,19 @@ class Stt:
     def get_transcribe_msg_by_id_list(self, transcribe_id_list):
         access_token = self.__get_access_token()
         headers = {'Authorization': 'bearer ' + access_token}
-        #msg_list = []
+        msg_list = []
         for transcribe_id in transcribe_id_list:
             result = self.__get_transcription_result(transcribe_id, headers)
             # 변환 성공
             if result['status'] == 'completed':
                 if not result['results']['utterances']:
                     continue   
-                self.stt_response.text.append(result['results']['utterances'][0]['msg'])
-                #msg_list.append(result['results']['utterances'][0]['msg'])
+                msg_list.append(result['results']['utterances'][0]['msg'])
             # 변환 실패
             else:
                 raise RuntimeError("STT 변환에 실패했습니다.")
-        return self.stt_response.text
-
+        return msg_list
+    
     # redis에서 token을 찾고 없으면 api 요청 후 redis에 저장한다.
     def __get_access_token(self):
         access_token = self.redis_client.get('access_token')
